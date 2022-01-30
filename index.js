a19784718c4f7460df7484821c154ecd7f362dee
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2022-01-29 23:11:56 -0500

    anyway so basically i added some user auth stuff thats gonna break if i do anything
    ignore the private key in the repo, it won't work anyway

--- index.js
+++ index.js
@@ -1,34 +1,138 @@
 // MAIN SCRIPT
 
-const port = 7326;
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const compression = require('compression');
-const mysql = require('mysql');
+const port = 8080;
+const saltRounds = 10;
+
+let validKeys = [];
+
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const mysql = require("mysql");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+const bcrypt = require("bcrypt");
+const { Server } = require("socket.io");
+const { uuid } = require("uuidv4");
+
 const gamedriver = require("./gamedriver.js");
+const userdriver = require("./userdriver.js");
 
 const app = express();
-
-
-
-
-
+const httpServer = http.createServer(app);
+// const httpsServer = https.createServer(
+//   {
+//     key: fs.readFileSync("key.pem"),
+//     cert: fs.readFileSync("cert.pem"),
+//   },
+//   app
+// );
+const io = new Server(httpServer);
 app.use(compression());
 app.use(cookieParser());
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
-    console.log(req.cookies);
-    res.render('pages/index');
+app.get("/", function (req, res) {
+  var cookies = req.cookies;
+  if (cookies.username != null) {
+    var ckey;
+    validKeys.forEach((key) => {
+      if (key.username == cookies.username) {
+        ckey = key;
+      }
+    });
+    if (ckey != null) {
+      //this won't scale well. good thing i don't need to worry about that
+      bcrypt.compare(ckey.key, cookies.authkey, (err, bres) => {
+        if (bres) {
+          console.log("user has valid key!");
+          res.redirect("/games");
+        } else {
+          console.log("user has invalid key!");
+          res.redirect("/sign");
+        }
+      });
+    } else {
+      console.log("user has outdated key!");
+      res.redirect("/sign");
+    }
+  } else {
+    res.render("pages/index");
+  }
 });
 
-app.get('/sign', function(req, res) {
-    res.render('pages/sign');
+app.get("/sign", function (req, res) {
+  res.render("pages/sign");
 });
-app.get('/games', function(req, res) {
-    res.render('pages/games');
+app.get("/games", function (req, res) {
+  res.render("pages/games");
 });
-app.listen(7326);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("signup", (msg) => {
+    var username = msg.username;
+    bcrypt.hash(msg.password, saltRounds, function (err, hash) {
+      StorePass(username, hash, socket);
+    });
+  });
+  socket.on("signin", (msg) => {
+    var username = msg.username;
+    CheckPass(username)
+      .then((v) => {
+        let usr = v[0];
+        if (usr) {
+          bcrypt.compare(msg.password, usr.hash, function (err, res) {
+            if (res) {
+              console.log(username + " logged in");
+              socket.emit("signin", "200");
+              MakeAuth(username, socket);
+            } else {
+              socket.emit("signin", "nopass");
+            }
+          });
+        } else {
+          socket.emit("signin", "nouser");
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log("HTTP Server running on port 80");
+});
+// httpsServer.listen(443, () => {
+//   console.log("HTTPS Server running on port 443");
+// });
+
+function StorePass(user, hash, socket) {
+  userdriver.getUser(user).then((v) => {
+    if (v.length > 0) {
+      socket.emit("signup", "taken");
+    } else {
+      userdriver.addUser(user, hash);
+      socket.emit("signup", "200");
+      MakeAuth(user, socket);
+    }
+  });
+}
+function MakeAuth(user, socket) {
+  let kpass = uuid();
+  bcrypt.hash(kpass, saltRounds, function (err, hash) {
+    var key = hash;
+    validKeys.push({
+      key: kpass,
+      username: user,
+    });
+    socket.emit("authkey", key);
+  });
+}
+function CheckPass(user) {
+  return userdriver.getUser(user);
+}
---
+++ userdriver.js
@@ -0,0 +1,84 @@
+const fs = require("fs");
+const dbFile = "users.db";
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require("sqlite3").verbose();
+const dbWrapper = require("sqlite");
+let db;
+dbWrapper
+  .open({
+    filename: dbFile,
+    driver: sqlite3.Database,
+  })
+  .then(async (dBase) => {
+    db = dBase;
+
+    try {
+      console.log(await db.all("SELECT * from Users"));
+      await db.run(
+        "CREATE TABLE Users (username TEXT, hash TEXT, permission TEXT)"
+      );
+    } catch (dbError) {}
+  });
+
+// Server script calls these methods to connect to the db
+module.exports = {
+  // Get the messages in the database
+  getUser: async (username) => {
+    try {
+      return await db.all("SELECT * from Users WHERE username = ?", username);
+    } catch (dbError) {
+      console.error(dbError);
+    }
+  },
+  // Add new message
+  addUser: async (username, hash) => {
+    let success = false;
+    try {
+      success = await db.run(
+        "INSERT INTO Users (username,hash,permission) VALUES (?,?,?)",
+        [username, hash, "None"]
+      );
+    } catch (dbError) {
+      console.error(dbError);
+    }
+    return success.changes > 0 ? true : false;
+  },
+  updateUser: async (username, permission) => {
+    let success = false;
+    try {
+      success = await db.run(
+        "Update Users SET permission = ? WHERE username = ?",
+        [username, hash, "None"]
+      );
+    } catch (dbError) {
+      console.error(dbError);
+    }
+    return success.changes > 0 ? true : false;
+  },
+
+  //   // Update message text
+  //   updateMessage: async (id, message) => {
+  //     let success = false;
+  //     try {
+  //       success = await db.run(
+  //         "Update Messages SET message = ? WHERE id = ?",
+  //         message,
+  //         id
+  //       );
+  //     } catch (dbError) {
+  //       console.error(dbError);
+  //     }
+  //     return success.changes > 0 ? true : false;
+  //   },
+
+  //   // Remove message
+  //   deleteMessage: async id => {
+  //     let success = false;
+  //     try {
+  //       success = await db.run("Delete from Messages WHERE id = ?", id);
+  //     } catch (dbError) {
+  //       console.error(dbError);
+  //     }
+  //     return success.changes > 0 ? true : false;
+  //   }
+};
