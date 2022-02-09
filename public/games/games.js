ad9f1550a18d6ffd130d17d7d577d7d17f5d4a65
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2022-02-09 15:29:01 -0500

    v;odhguduf

--- public/games/games.js
+++ public/games/games.js
@@ -1,9 +1,62 @@
 const socket = io();
 
+collections = [];
+
 socket.emit("alive");
 $(document).ready(() => {
     setInterval(() => {
         socket.emit("alive")
     }, 10000);
-
+    socket.emit("games", {
+        type: "fetch"
+    })
 });
+socket.on("games", req => {
+    switch (req.type) {
+        case "fetch":
+            console.log(req);
+            req.data.forEach(collection => {
+                console.log(collection);
+                collections.push(new Collection(collection));
+            });
+            break;
+    }
+})
+
+class Collection {
+    constructor(data) {
+        this.name = data.name;
+        this.collection = this.appendCollection();
+
+        data.games.forEach(g => {
+            this.appendGame(g);
+        });
+    }
+    appendGame(g) {
+        let game = $("<div class='game'>");
+
+        this.collection.children().append(game);
+        game.text(g.name);
+        game.click(() => {
+            $("#mainframe").prop("src", g.url);
+        });
+    }
+    appendCollection() {
+        let selbar = $("#selbar");
+        let outer = $("<div class='m-text t3 col-md1'>");
+        let inner = $("<div class='m-hidden'>");
+
+        outer.text(this.name + " >");
+        outer.click(() => {
+            if (inner[0].classList.contains("m-hidden")) {
+                inner.removeClass("m-hidden");
+            } else {
+                inner.addClass("m-hidden");
+            }
+        });
+
+        selbar.append(outer);
+        return outer;
+    }
+
+}
